e8ee7f961efbcc4a4622ad058755efd25b3b755d
vasyapeteckin <unik128@gmail.com>
2023-05-04 15:59:21 +0300

    version 0.0.1

--- src/main.py
+++ src/main.py
@@ -1,13 +1,39 @@
 import os
+import sys
 
 from pyrogram import Client
+from pyrogram.enums import ChatType
 from dotenv import load_dotenv
 
-load_dotenv()
+from utils import create_html
 
+load_dotenv()
 api_id = os.getenv('API_ID')
 api_hash = os.getenv('API_HASH')
 
 app = Client("my_account", api_id=api_id, api_hash=api_hash)
 
-app.run()
+
+async def search_message_in_chats(target_user):
+    result = []
+    async with app:
+        async for dialog in app.get_dialogs():
+            if dialog.chat.type == ChatType.SUPERGROUP:
+                chat_title, chat_id, chat_username = dialog.chat.first_name or dialog.chat.title, dialog.chat.id, dialog.chat.username
+                # print(chat_title, chat_username)
+                data = {'id': chat_id,
+                        'title': chat_title,
+                        'username': chat_username,
+                        'messages': []}
+                async for message in app.search_messages(chat_id=chat_id, from_user=target_user):
+                    # print(message.date, message.chat.title, message.text.replace('\n', ' ') if message.text is not None else None)
+                    data['messages'].append(message)
+                if data['messages']:
+                    result.append(data)
+    create_html(target_user=target_user, chats=result)
+
+
+if __name__ == '__main__':
+    target_user = ''
+
+    app.run(search_message_in_chats(target_user=target_user))
---
+++ src/utils.py
@@ -0,0 +1,15 @@
+from jinja2 import Environment, FileSystemLoader
+
+
+def create_html(**kwargs):
+    env = Environment(loader=FileSystemLoader('templates'))
+    template = env.get_template('main.html')
+    output_from_parsed_template = template.render(kwargs)
+
+    with open(f"{kwargs['target_user']}.html", "w", encoding="utf-8") as f:
+        f.write(output_from_parsed_template)
+
+
+if __name__ == '__main__':
+    # create_html(target_name="vaas=ya", chats=[{'title': '1'}, {'title': '2'}, {'title': '3'}, {'title': '4'}])
+    ...
